ff614e05cb2f8221f6d7dd712efb81141930ad04
VMoises <moisesvilladiegos@gmail.com>
2022-06-11 15:15:17 -0500

    Refactorizacion

--- fibonacci.py
+++ fibonacci.py
@@ -1,2 +1,6 @@
 def fibonacci(numero):
-  return 0
+  if numero == 1:
+    return 0
+  if numero == 2:
+    return 1
+  return fibonacci(numero - 1) + fibonacci(numero - 2)
--- test_fibonacci.py
+++ test_fibonacci.py
@@ -2,3 +2,15 @@ from fibonacci import fibonacci
 
 def test_fibonacci1():
   assert fibonacci(1) == 0
+
+def test_fibonacci2():
+  assert fibonacci(2) == 1
+
+def test_fibonacci3():
+  assert fibonacci(3) == 1
+
+def test_fibonacci4():
+  assert fibonacci(4) == 2
+
+def test_fibonacci6():
+  assert fibonacci(6) == 5
